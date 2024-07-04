68721dc1092e1000759cc622c240e058dbe4b2ca
immanu10 <54578417+immanu10@users.noreply.github.com>
2024-07-04 13:53:00 +0530

    react, vite, tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function App() {
+  // const [isDark, setIsDark] = useState(false);
+
+  // const toggleDarkMode = () => {
+  //   setIsDark(!isDark);
+  //   document.body.classList.toggle("dark");
+  // };
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
